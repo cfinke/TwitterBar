2d3f63b82b7565958b965206f2f8d9a39de77c00
cfinke <cfinke@gmail.com>
2009-07-30 19:40:15 +0000

    Moving TwitterBar root

---
+++ twitterbar/chrome/content/options.js
@@ -0,0 +1,42 @@
+var TWITTERBAR_OPTIONS = {
+	get prefs() { return Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).QueryInterface(Components.interfaces.nsIPrefBranch).getBranch("extensions.twitter."); },
+	
+	get strings() { return document.getElementById("twitterbar-strings"); },
+	
+	init : function () {
+	    var authDate = this.prefs.getCharPref("oauth_timestamp");
+	    
+	    if (authDate) {
+	        var niceDate = new Date();
+	        niceDate.setTime(authDate);
+	        
+	        document.getElementById("auth-summary").textContent = this.strings.getFormattedString("twitterbar.newAuthString", [ niceDate.toLocaleString() ]);
+        }
+        else {
+            document.getElementById("auth-summary").textContent = this.strings.getString("twitterbar.noAuth");
+        }
+        
+        sizeToContent();
+    },
+	
+	accept : function () {
+	    if (!document.getElementById("twitterbar-preference-window").instantApply) {
+		    this.prefs.setBoolPref("confirm", document.getElementById("pref-confirm").value);
+		    this.prefs.setCharPref("web", document.getElementById("pref-prefix").value);
+		    this.prefs.setBoolPref("pref-open-after", document.getElementById("pref-open-after").value);
+		    this.prefs.setBoolPref("pref-hide-button", document.getElementById("pref-hide-button").value);
+		    this.prefs.setCharPref("shortener", document.getElementById("pref-shortener").value);
+		    this.prefs.setBoolPref("showTrends", document.getElementById("pref-show-trends").value);
+		}
+		
+		return true;
+	},
+	
+	clearAuth : function () {
+        this.prefs.setCharPref("access_token.oauth_token", "");
+        this.prefs.setCharPref("access_token.oauth_token_secret", "");
+        this.prefs.setCharPref("oauth_timestamp", "");
+        
+        document.getElementById("auth-summary").textContent = this.strings.getString("twitterbar.noAuth");
+    }
+};
