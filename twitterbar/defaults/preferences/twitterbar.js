27851f246bd8cadfe3f13f8ec57287831870cc7f
cfinke <cfinke@gmail.com>
2009-08-31 00:59:24 +0000

    Moving directories to the right spot

---
+++ twitterbar/defaults/preferences/twitterbar.js
@@ -0,0 +1,20 @@
+pref("extensions.twitter.tab", false);
+pref("extensions.twitter.button", false);
+pref("extensions.twitter.web", "Currently Browsing: ");
+pref("extensions.twitter.version", "");
+pref("extensions.twitter.search_request", false);
+pref("extensions.twitter.search_fixed", false);
+
+pref("extensions.twitter.confirm", true);
+
+pref("extensions.twitter.access_token.oauth_token", "");
+pref("extensions.twitter.access_token.oauth_token_secret", "");
+
+pref("extensions.twitter.oauth_timestamp", "");
+
+pref("extensions.twitter.shortener", "tk");
+
+pref("extensions.twitter.trends", "");
+pref("extensions.twitter.trends.update", "");
+
+pref("extensions.twitter.showTrends", true);
